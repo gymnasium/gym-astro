3cc3485a651a5ea1cd57b0b9fa3a55a151c29fa2
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-04-06 11:37:57 -0400

    configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,33 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from 'astro-imagetools';
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import netlify from "@astrojs/netlify/functions";
+import react from "@astrojs/react";
+import yaml from '@rollup/plugin-yaml';
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    astroImageTools,
+    compress(),
+    sitemap(),
+    robotsTxt(),
+    react(),
+    // partytown({
+    //   // Example: Disable debug mode.
+    //   config: { 
+    //     debug: true,
+    //     forward: ["dataLayer.push"]
+    //   },
+  
+    // })
+  ],
+  output: "server",
+  adapter: netlify(),
+  vite: {
+    plugins: [yaml()]
+  }
+});
