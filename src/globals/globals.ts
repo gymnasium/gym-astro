d10c550be323e85fd100bc73e07d0d747688b21c
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-04-10 13:52:43 -0400

    Create globals.ts

---
+++ src/globals/globals.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Gymnasium';
+export const SITE_AUTHOR = 'Aquent Gymnasium';
+export const SITE_DESCRIPTION = 'Design a career you love with free online courses on design, development, accessibility, prototyping, UX, and career skills.';
