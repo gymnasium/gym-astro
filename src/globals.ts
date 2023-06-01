f226c9cea6821d170d51923a05ae69fada895d78
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-06-01 16:37:58 -0400

    Use JSON data to render

--- src/globals.ts
+++ src/globals.ts
@@ -1,8 +1,12 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Gymnasium';
-export const SITE_AUTHOR = 'Aquent Gymnasium';
-export const SITE_DESCRIPTION = 'Design a career you love with free online courses on design, development, accessibility, prototyping, UX, and career skills.';
-export const CMS_URL = 'https://data--thegymcms.netlify.app'
+export const CMS_URL = import.meta.env.DEV ? 'http://localhost:4000' : 'https://data.gym.soy'
 export const FEED_URL = `${CMS_URL}/feeds/complete.json`
+export const DATA = await fetch(FEED_URL).then((response) => response.json());
+export const BIOS = await DATA.items.bios;
+export const CONFIG = await DATA.items.config;
+export const META = await DATA.items.config.meta;
+export const NAVIGATION = await DATA.items.config.navigation;
+export const NAV_MAIN = await DATA.items.config.navigation.main;
+export const NAV_FOOTER = await DATA.items.config.navigation.footer;
+export const LMS_URL = await CONFIG.lms_url
+export const ASTRO_URL = await CONFIG.astro_url
+export const ELEVENTY_URL = await CONFIG.eleventy_url
