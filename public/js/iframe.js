a1f3d833f7a61ee59bf3ff2cf25cd64e304c434f
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-10-18 09:40:59 -0400

    Update iframe.js

--- public/js/iframe.js
+++ public/js/iframe.js
@@ -18,7 +18,7 @@ if (window.top !== window.self) {
     }
 
     console.log(`message received: ${event.data}`,event);
-    const message = JSON.stringify({'auth': document.documentElement.getAttribute('data-auth'), 'cookie': document.cookie});
+    const message = JSON.stringify({'auth': document.documentElement.getAttribute('data-auth'), 'cookies': document.cookie});
 
     window.top.postMessage(message,event.origin);
   },false);
